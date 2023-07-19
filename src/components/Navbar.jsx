a8873779471e3f9633530be2b4cd6349b53458d2
ChandanPk <chandanpk9@gmail.com>
2023-07-19 13:26:52 +0530

    Add tailwind-css lib and complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div class="mx-auto bg-red-200 grid grid-cols-2 p-3">
+      <div>Merchendise</div>
+      <ul class="flex justify-end bg-slate-100 gap-x-3 hover:cursor-pointer">
+        <li>Home</li>
+        <li>Orders</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
